d723f2c2f0d248446a2903212f0c3482eb719459
YouliangZhang <1577121881@qq.com>
2019-12-01 00:16:58 +0800

    改用 pipenv

--- api/__init__.py
+++ api/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config['JSON_AS_ASCII'] = False
-
     from .views import app as main_app
     app.register_blueprint(main_app)
 
